13a59ea3e47bc241ee60028a44ea019a97559c22
Mykola Ananiichuk <Kolya-an@users.noreply.github.com>
2025-10-08 16:23:53 +0300

    First commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@sss.ss',
+            'password' => '11111111',
+            'role' => 'admin',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Pac',
+            'email' => 'pac@sss.ss',
+            'password' => '11111111',
+            'role' => 'patient',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Doc',
+            'email' => 'doc@sss.ss',
+            'password' => '11111111',
+            'role' => 'doctor',
+        ]);
+    }
+}
