f2d7a19d104ddd5e882ec28033ce13c5613fd469
Mykola Ananiichuk <Kolya-an@users.noreply.github.com>
2025-12-08 18:19:13 +0200

    Додав посилання на телеграм

--- app/Filament/Resources/Doctors/Schemas/DoctorForm.php
+++ app/Filament/Resources/Doctors/Schemas/DoctorForm.php
@@ -72,7 +72,7 @@ class DoctorForm
                         FileUpload::make('photo')
                             ->label('Фото')
                             ->directory('doctor/' . date('Y') . '/' . date('m'))
-                            ->disk('public')
+                            ->disk('public_uploads')
                             ->visibility('public')
                             ->image()
                             ->imageEditor()
