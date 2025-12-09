f769066a542707cbf6f56ce0ab3fabfc6ff73d3e
Mykola Ananiichuk <Kolya-an@users.noreply.github.com>
2025-12-09 10:28:20 +0200

    Додав посилання на телеграм

--- app/Livewire/Doctor/Sex.php
+++ app/Livewire/Doctor/Sex.php
@@ -9,16 +9,57 @@ class Sex extends Component
 {
     public $user;
     public $sex;
+    public $name;
+    public $second_name;
     public $search = '';
+
+    protected $rules = [
+        'name' => 'required|string|max:255',
+        'second_name' => 'nullable|string|max:255',
+    ];
     public function mount()
     {
         $this->user = Auth::user();
 
         $user = Auth::user();
-
+        $this->name = $user->name;
         if ($user->doctor) {
             $this->sex = $user->doctor->sex;
+            $this->second_name = $user->doctor->second_name;
+        } else {
+            $this->second_name = '';
         }
+    }
+    public function updatedName($value)
+    {
+        // Валідація лише для поля 'name'
+        $this->validateOnly('name');
+
+        // Оновлення name в таблиці users
+        $this->user->update(['name' => $value]);
+
+        // Опціонально: вивести повідомлення про успіх
+        //session()->flash('name_message', 'Ім\'я оновлено успішно.');
+    }
+
+    // Метод оновлення для second_name (викликається після зміни поля)
+    public function updatedSecondName($value)
+    {
+        // Валідація лише для поля 'second_name'
+        $this->validateOnly('second_name');
+
+        $user = $this->user; // Використовуємо властивість $this->user
+
+        if ($user->doctor) {
+            // Оновлення second_name в таблиці doctors
+            $user->doctor->update(['second_name' => $value]);
+        } else {
+            // Створення запису doctor та встановлення second_name
+            $user->doctor()->create(['second_name' => $value, 'sex' => $this->sex ?? 'male']); // Встановлюємо sex за замовчуванням, якщо він ще не обраний
+        }
+
+        // Опціонально: вивести повідомлення про успіх
+        //session()->flash('second_name_message', 'Прізвище оновлено успішно.');
     }
     public function updateSex($value)
     {
--- resources/views/livewire/doctor/sex.blade.php
+++ resources/views/livewire/doctor/sex.blade.php
@@ -1,4 +1,20 @@
 <div class="spec_register1_left_block">
+    <div class="doctor_name">
+        <input
+            id="add_name"
+            class="client_add_name"
+            type="text"
+            wire:model.live="name"
+            placeholder="Ваше ім'я"
+        />
+        <input
+            id="add_second_name"
+            class="client_add_second_name"
+            type="text"
+            wire:model.live="second_name"
+            placeholder="{{__('Ваше прізвище')}}"
+        />
+    </div>
     <h5>{{__('Ваша стать')}}</h5>
     <label class="custom-radio">
         <input type="radio" wire:model="sex" wire:change="updateSex('female')" value="female">
