4d8b177e5f1002c7d8d0a246df72c6d2c06c7d13
Mykola Ananiichuk <Kolya-an@users.noreply.github.com>
2026-01-23 12:20:56 +0200

    Відправка пошти

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+    <meta name="robots" content="noindex, nofollow">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('page.title', config('app.name'))</title>
     <meta name="description" content="@yield('page.description', $pageDescription ?? '')">
